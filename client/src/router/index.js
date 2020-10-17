9428114a1473fa6f2f9021b4b21342824fc192f3
Alimul Khakim <alimulkhakim@gmail.com>
2020-10-17 14:46:31 +0700

    add navigation guards

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -36,5 +36,12 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  if(to.path === '/' && !localStorage.getItem('access_token')) {
+    next({ path: '/login' })
+  } else {
+    next()
+  }
+})
 
 export default router
